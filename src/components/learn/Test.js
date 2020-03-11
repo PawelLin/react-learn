edb3043d37d151dd7b159df73144357661935210
pawel <linpeiwei@f-road.com.cn>
2020-03-11 18:05:03 +0800

    learn 3-day

--- src/components/learn/Test.js
+++ src/components/learn/Test.js
@@ -19,47 +19,105 @@ function ProductRow (props) {
 
 function ProductCategoryRow (props) {
     return <tr>
-        <td colSpan="2">{props.category}</td>
+        <th colSpan="2" align="left">{props.category}</th>
     </tr>
 }
 
 function ProductTable (props) {
     const products = props.products
-    return <table>
-        <thead>
-            <th>Name</th>
-            <th>Price</th>
-        </thead>
-        <tbody>
-            {products.map((list, index) => <Fragment key={list[0].name + index}>
-                <ProductCategoryRow category={list[0].category} />
-                {list.map(item => <ProductRow {...item} key={item.name} />)}
-            </Fragment>)}
-        </tbody>
-    </table>
-}
-
-class FilterableProductTable extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            search: '',
-            inStockOnly: false
-        }
-    }
-    render () {
-        const list = []
-        const key = []
-        data.forEach(item => {
+    const lists = []
+    const key = []
+    const { filterText, inStockOnly } = props
+    products.forEach(item => {
+        if ((!filterText || item.name.includes(filterText)) && (!inStockOnly || item.stocked)) {
             const index = key.indexOf(item.category)
             if (index === -1) {
                 key.push(item.category)
-                list[list.length] = [item]
+                lists[lists.length] = [item]
             } else {
-                list[index].push(item)
+                lists[index].push(item)
             }
+        }
+    })
+    return <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                lists.map((list, index) =>
+                    <Fragment key={list[0].name + index}>
+                        <ProductCategoryRow category={list[0].category} />
+                        {list.map(item => <ProductRow {...item} key={item.name} />)}
+                    </Fragment>
+                )
+            }
+        </tbody>
+        {
+            !lists.length && 
+            <tfoot>
+                <tr>
+                    <th colSpan="2">No data</th>
+                </tr>
+            </tfoot>
+        }
+    </table>
+}
+
+class SearchBar extends React.Component {
+    onFilterTextChange = e => {
+        this.props.onFilterTextChange(e.target.value)
+    }
+    onInStockChange = e => {
+        this.props.onInStockChange(e.target.checked)
+    }
+    render () {
+        return <div>
+            <input value={this.props.search} onChange={this.onFilterTextChange} type="text" placeholder="Search..." />
+            <p>
+                <input checked={this.props.inStockOnly} onChange={this.onInStockChange} type="checkbox" />
+                <label>Only Show products in stock</label>
+            </p>
+        </div>
+    }
+}
+
+class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        }
+    }
+    handleFilterTextChange = filterText => {
+        this.setState({
+            filterText
         })
-        return <ProductTable products={list} />
+    }
+    handleInStockChange = inStockOnly => {
+        this.setState({
+            inStockOnly
+        })
+    }
+    render () {
+        return <fieldset>
+            <legend>Test</legend>
+            <SearchBar
+                filterText={this.state.filterText}
+                inStockOnly={this.state.inStockOnly}
+                onFilterTextChange={this.handleFilterTextChange}
+                onInStockChange={this.handleInStockChange}
+            />
+            <ProductTable
+                products={data}
+                filterText={this.state.filterText}
+                inStockOnly={this.state.inStockOnly}
+            />
+        </fieldset>
     }
 }
 
